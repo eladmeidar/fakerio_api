ffa7517bc43d39a321ecda27465078cf5898d350
Eric Chen <eric87chen@gmail.com>
2014-06-24 15:05:27 -0400

    [FEAT]Added ip address to internet

--- lib/fakerio_api/internet.rb
+++ lib/fakerio_api/internet.rb
@@ -2,9 +2,15 @@ module FakerioApi
   class Internet
 
     def self.emails(options = {})
-      request_parameteres = {count: 1}.merge(options)
-      request_parameteres.merge!({uuid: FakerioApi::Base.uuid, auth_token: FakerioApi::Base.token})
-      FakerioApi::Base.get("/api/v1/internet/emails.json", body: request_parameteres).parsed_response
+      request_parameters = {count: 1}.merge(options)
+      request_parameters.merge!({uuid: FakerioApi::Base.uuid, auth_token: FakerioApi::Base.token})
+      FakerioApi::Base.get("/api/v1/internet/emails.json", body: request_parameters).parsed_response
+    end
+
+    def self.ip_v4_addresses(opitons = {})
+      request_parameters = {count: 1}.merge(options)
+      request_parameters.merge!({uuid: FakerioApi::Base.uuid, auth_token: FakerioApi::Base.token})
+      FakerioApi::Base.get("/api/v1/internet/ip_addresses.json", body: request_parameters).parsed_responpse
     end
   end
 end
